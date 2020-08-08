a2e92caa59b733c72d56505fe3f1ff63d7cc1af5
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2020-08-08 13:13:45 -0700

    Add tests for number of times methods hit database

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,8 +1,10 @@
 require './user.rb'
+require './questions_database.rb'
 
 describe User do
   describe '.find_by_name' do
     it 'returns User object that matches parameters' do
+      expect(QuestionsDatabase.instance).to receive(:execute).exactly(1).times.and_call_original
       user_obj = User.find_by_name('Andrew', 'Hayhurst')
       expect(user_obj).to be_a(User)
       expect(user_obj.fname).to eq('Andrew')
@@ -14,11 +16,13 @@ describe User do
     subject { User.find_by_id(4) }
 
     it 'returns Question object(s) for all authored questions' do
+      expect(QuestionsDatabase.instance).to receive(:execute).exactly(3).times.and_call_original
       expect(subject.authored_questions.length).to eq(2)
       expect(subject.authored_questions[0].title).to eq('Giraffes')
     end
 
     it 'returns empty array if no authored questions' do
+      expect(QuestionsDatabase.instance).to receive(:execute).exactly(2).times.and_call_original
       uninquisitive_user = User.find_by_id(5)
       expect(uninquisitive_user.authored_questions).to eq([])
     end
@@ -28,6 +32,7 @@ describe User do
     subject { User.find_by_id(1) }
 
     it 'returns Reply object(s) for all authored replies' do
+      expect(QuestionsDatabase.instance).to receive(:execute).exactly(3).times.and_call_original
       expect(subject.authored_replies[1].parent_reply).to eq(1)
       expect(subject.authored_replies[2].body).to eq('Pancakes')
     end
@@ -37,6 +42,7 @@ describe User do
     subject { User.find_by_id(2) }
 
     it 'returns Question object(s) for all followed questions' do
+      expect(QuestionsDatabase.instance).to receive(:execute).exactly(2).times.and_call_original
       expect(subject.followed_questions.map(&:title)).to contain_exactly(
         'Snowden', 'Giraffes'
       )
@@ -47,6 +53,7 @@ describe User do
     subject { User.find_by_id(6) }
 
     it 'returns Question object(s) for all liked questions' do
+      expect(QuestionsDatabase.instance).to receive(:execute).exactly(2).times.and_call_original
       expect(subject.liked_questions.map(&:title)).to contain_exactly(
         'Breakfast', 'Giraffes', 'Yesterday'
       )
@@ -55,6 +62,7 @@ describe User do
 
   describe '#average_karma' do
     it "returns average number of likes for user's questions" do
+      expect(QuestionsDatabase.instance).to receive(:execute).exactly(6).times.and_call_original
       expect(User.find_by_id(4).average_karma).to eq(3)
       expect(User.find_by_id(3).average_karma).to eq(1)
       expect(User.find_by_id(1).average_karma).to eq(0)
